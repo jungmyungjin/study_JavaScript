ae51d0dceffecff81cf56f137f1383a624f92a7b
jungmyungjin <myung2667@gmail.com>
2020-01-16 22:36:08 +0900

    [nomadcoders] 변수의 타입 (string, bullean, number, float, array, object)
    [nomadcoders - 바닐라 JS로 크롬 앱 만들기]
    (string, bullean, number, float, array, object)

---
+++ 003-Data Types on JS/index.js
@@ -0,0 +1,21 @@
+// 처음 할땐 무조건 const로, 진짜 필요할때만 let을 사용한다!
+
+/* String
+// const what1 = Nicolas; // Uncaught ReferenceError: Nicolas is not defined (what과 Nicolas는 변수명으로 인식됨. 때문에 Nicolas라는 변수가 존재하지 않는데 사용하려해서 오류가 남)
+const what2 = "Nicolas";
+console.log(what2);
+*/
+
+/* Boolean
+const wat = ture;
+*/
+
+/* Number
+const wat =666;
+*/
+
+// Folat
+const wat = 55.1;
+
+
+console.log(wat);
---
+++ 004-Organizing Data with Arrays/index.js
@@ -0,0 +1,28 @@
+
+
+// 변수명 : Camel case 형식
+let DaysOfWeek = "aa";
+
+// Array : List 형태로 데이터를 저장
+DaysOfWeek = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun',true];
+
+
+console.log(DaysOfWeek);
+
+
+// Object : 각 value 별로 이름을 지정할 수 있는 List 형태
+const myInfo = {
+    name: "myungjin",
+    age: 25,
+    isLovely:true
+};
+
+console.log(myInfo);
+
+// Array와 Object를 합칠 수 있다.
+const favoriteFood={
+    drink:[{tea:"olong", soda:"chillseong styleMedia"}],
+    food:[{soup:"kimch zzigae"}]
+}
+
+console.log(favoriteFood);
